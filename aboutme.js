5b16e8a93524b7abacf1af37f5234bcf33a14067
Rachel Burak <rachelburak@yahoo.com>
2019-07-09 14:58:36 -0700

    Initial Commit

---
+++ aboutme.js
@@ -0,0 +1,10 @@
+alert("Hello!");
+
+function bigImg(x){
+  x.style.height = "406px";
+  x.style.width = "606px";
+}
+function originalImg(x){
+  x.style.height = "306px";
+  x.style.width = "506px";
+}
